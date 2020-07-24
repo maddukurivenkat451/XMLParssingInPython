a22ece8bf8ab02c367abc2e9b06505af98697c08
maddukurivenkat451 <66637269+maddukurivenkat451@users.noreply.github.com>
2020-07-24 17:16:15 -0400

    Add files via upload

---
+++ SAMPLE.py
@@ -0,0 +1,30 @@
+# Parse XML with etree
+tree = et.XML("""<?xml version="1.0"?>
+<data>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+        <neighbor name="Austria" direction="E"/>
+        <neighbor name="Switzerland" direction="W"/>
+        <neighbor2 name="Italy" direction="S"/>
+    </country>
+    <country name="Singapore">
+        <rank>4</rank>
+        <year>2011</year>
+        <gdppc>59900</gdppc>
+        <neighbor name="Malaysia" direction="N"/>
+        <cities>
+            <city name="Chargin" population="1234" />
+            <city name="Firin" population="4567" />
+        </cities>
+    </country>
+    <country name="Panama">
+        <rank>68</rank>
+        <year>2011</year>
+        <gdppc>13600</gdppc>
+        <neighbor name="Costa Rica" direction="W"/>
+        <neighbor name="Colombia" direction="E"/>
+    </country>
+</data>
+""")
